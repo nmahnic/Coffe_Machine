6f0710920f0b6d76c3a8df34e81dfacdfcc74ee9
Nicolás Mahnic <nmahnic@pdssa.com>
2020-12-31 05:48:43 -0300

    stage 5 complete

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -4,6 +4,11 @@ import kotlin.math.roundToInt
 import kotlin.math.truncate
 
 fun main() {
+    val coffe = CoffeeMachine()
+    coffe.makeCoffe()
+}
+
+class CoffeeMachine(){
     val input = Scanner(System.`in`)
 
     var waterStock : Int  = 400
@@ -25,108 +30,123 @@ fun main() {
     val coffeeNeededC : Int  = 12
     val priceC : Int = 6
 
-    var water : Int
-    var milk : Int
-    var coffee : Int
-    var cups: Int
+    var water : Int = 0
+    var milk : Int = 0
+    var coffee : Int = 0
+    var cups: Int = 0
     var N : Int? = 0
-    var option : String
-    var optionCoffe : Int
+    var option : String = ""
+    var optionCoffe : String = ""
 
-    println("The coffee machine has:")
-    println("${waterStock} of water")
-    println("${milkStock} of milk")
-    println("${coffeeStock} of coffee beans")
-    println("${cupsStock} of disposable cups")
-    println("${money} of money\n")
+    fun makeCoffe() {
+        option = " "
 
-    option = ""
-
-    while(option != "exit"){
-        print("Write action (buy, fill, take, remaining, exit):")
-        option = input.nextLine()
-        when (option) {
-            "buy" -> {
-                print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-                optionCoffe = input.nextInt()
-
-                when (optionCoffe) {
-                    1 -> {
-                        waterStock -= waterNeededE
-                        milkStock -= milkNeededE
-                        coffeeStock -= coffeeNeededE
-                        cupsStock -= 1
-                        money += priceE
+        while (option != "exit") {
+            print("\nWrite action (buy, fill, take, remaining, exit): ")
+            option = input.next()
+            when (option) {
+                "buy" -> {
+                    print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+                    optionCoffe = input.next()
+                    if(checkForDisponibility(optionCoffe)) {
+                        when (optionCoffe) {
+                            "1" -> {
+                                waterStock -= waterNeededE
+                                milkStock -= milkNeededE
+                                coffeeStock -= coffeeNeededE
+                                cupsStock -= 1
+                                money += priceE
+                            }
+                            "2" -> {
+                                waterStock -= waterNeededL
+                                milkStock -= milkNeededL
+                                coffeeStock -= coffeeNeededL
+                                cupsStock -= 1
+                                money += priceL
+                            }
+                            "3" -> {
+                                waterStock -= waterNeededC
+                                milkStock -= milkNeededC
+                                coffeeStock -= coffeeNeededC
+                                cupsStock -= 1
+                                money += priceC
+                            }
+                            "back" -> continue
+                            else -> println("ERROR")
+                        }
                     }
-                    2 -> {
-                        waterStock -= waterNeededL
-                        milkStock -= milkNeededL
-                        coffeeStock -= coffeeNeededL
-                        cupsStock -= 1
-                        money += priceL
-                    }
-                    3 -> {
-                        waterStock -= waterNeededC
-                        milkStock -= milkNeededC
-                        coffeeStock -= coffeeNeededC
-                        cupsStock -= 1
-                        money += priceC
-                    }
-                    else -> println("ERROR")
                 }
+                "fill" -> {
+                    print("Write how many ml of water do you want to add: ")
+                    water = input.nextInt()
+                    print("Write how many ml of milk do you want to add: ")
+                    milk = input.nextInt()
+                    print("Write how many grams of coffee beans do you want to add: ")
+                    coffee = input.nextInt()
+                    print("Write how many disposable cups of coffee do you want to add: ")
+                    cups = input.nextInt()
 
-                println("\nThe coffee machine has:")
-                println("${waterStock} of water")
-                println("${milkStock} of milk")
-                println("${coffeeStock} of coffee beans")
-                println("${cupsStock} of disposable cups")
-                println("${money} of money")
-            }
-            "fill" -> {
-                print("Write how many ml of water do you want to add:")
-                water = input.nextInt()
-                print("Write how many ml of milk do you want to add:")
-                milk = input.nextInt()
-                print("Write how many grams of coffee beans do you want to add:")
-                coffee = input.nextInt()
-                print("Write how many disposable cups of coffee do you want to add:")
-                cups = input.nextInt()
+                    waterStock += water
+                    milkStock += milk
+                    coffeeStock += coffee
+                    cupsStock += cups
 
-                waterStock += water
-                milkStock += milk
-                coffeeStock += coffee
-                cupsStock += cups
-
-                println("\nThe coffee machine has:")
-                println("${waterStock} of water")
-                println("${milkStock} of milk")
-                println("${coffeeStock} of coffee beans")
-                println("${cupsStock} of disposable cups")
-                println("${money} of money")
+                }
+                "take" -> {
+                    println("I gave you \$${money}")
+                    money -= money
+                }
+                "remaining" -> {
+                    println("\nThe coffee machine has:")
+                    println("${waterStock} of water")
+                    println("${milkStock} of milk")
+                    println("${coffeeStock} of coffee beans")
+                    println("${cupsStock} of disposable cups")
+                    println("\$${money} of money")
+                }
+                "exit" -> {
+                    break
+                }
             }
-            "take" -> {
-                println("I gave you \$${money}")
-                money -= money
-                println("\nThe coffee machine has:")
-                println("${waterStock} of water")
-                println("${milkStock} of milk")
-                println("${coffeeStock} of coffee beans")
-                println("${cupsStock} of disposable cups")
-                println("${money} of money")
-            }
-            "remaining" -> {
-                println("\nThe coffee machine has:")
-                println("${waterStock} of water")
-                println("${milkStock} of milk")
-                println("${coffeeStock} of coffee beans")
-                println("${cupsStock} of disposable cups")
-                println("${money} of money")
-            }
-            "exit" -> {break}
-            else -> println("ERROR")
         }
     }
+    private fun checkForDisponibility(optionCoffe : String) : Boolean{
+        when(optionCoffe){
+            "1" ->{
+                if(waterStock < waterNeededE){ println("Sorry, not enough water!") }
+                if(milkStock < milkNeededE){ println("Sorry, not enough milk!") }
+                if(coffeeStock < coffeeNeededE){ println("Sorry, not enough coffee beans!") }
+                if((waterStock >= waterNeededE) && (milkStock >= milkNeededE) && (coffeeStock >= coffeeNeededE)){
+                    println("I have enough resources, making you a coffee!")
+                    return true
+                }
+            }
+            "2" ->{
+                if(waterStock < waterNeededL){ println("Sorry, not enough water!") }
+                if(milkStock < milkNeededL){ println("Sorry, not enough milk!") }
+                if(coffeeStock < coffeeNeededL){ println("Sorry, not enough coffee beans!") }
+                if((waterStock >= waterNeededL) && (milkStock >= milkNeededL) && (coffeeStock >= coffeeNeededL)){
+                    println("I have enough resources, making you a coffee!")
+                    return true
+                }
+            }
+            "3" ->{
+                if(waterStock < waterNeededC){ println("Sorry, not enough water!") }
+                if(milkStock < milkNeededC){ println("Sorry, not enough milk!") }
+                if(coffeeStock < coffeeNeededC){ println("Sorry, not enough coffee beans!") }
+                if((waterStock >= waterNeededC) && (milkStock >= milkNeededC) && (coffeeStock >= coffeeNeededC)){
+                    println("I have enough resources, making you a coffee!")
+                    return true
+                }
+            }
+            "back" -> return true
+            else -> return false
+        }
+        return false
+    }
 }
+
+
 
 /*
 var lista = listOf(((milk/milkNeeded)),(water/waterNeeded),(coffee/coffeeNeeded))
