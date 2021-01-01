22754022b0a9152a2cb1662554528e725e8da946
Nicolás Mahnic <nmahnic@pdssa.com>
2021-01-01 04:56:52 -0300

    class exercise

---
+++ Problems (3)/Composition/src/Main.kt
@@ -0,0 +1,20 @@
+// write the classes here
+class OperatingSystem{
+    lateinit var name : String
+}
+
+class DualBoot{
+    lateinit var primaryOs : OperatingSystem
+    lateinit var secondaryOs : OperatingSystem
+}
+
+/*
+class OperatingSystem {
+    var name: String = "theOS"
+}
+
+class DualBoot {
+    var primaryOs = OperatingSystem()
+    var secondaryOs = OperatingSystem()
+}
+*/
